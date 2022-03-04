ae0c78b8d4fa337882927de1724e0154827f0f56
Mike McGuire <mike.mcguire@mullenhigh.com>
2022-03-04 13:47:53 -0700

    First attempt

---
+++ Time and Time Again/src/TimeAndTimeAgain.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class TimeAndTimeAgain
+	{
+		public static void main(String[] args)
+			{
+			Scanner input = new Scanner(System.in);
+			int numberOfCases = input.nextInt();
+			for (int i = 0; i < numberOfCases; i++)
+				{
+				int hour = 0;
+				int minute = 0; 
+				int second = 0;
+				String data = input.nextLine();
+				
+				Pattern secondRegex = Pattern.compile(".*(//d+)s.*");
+				Matcher matcher1 = secondRegex.matcher(data);
+				if (matcher1.matches())
+					second = Integer.parseInt(matcher1.group(1));
+				
+				Pattern minuteRegex = Pattern.compile(".*(//d+)m.*");
+				Matcher matcher2 = minuteRegex.matcher(data);
+				if (matcher2.matches())
+					minute = Integer.parseInt(matcher2.group(1));
+				
+				Pattern hourRegex = Pattern.compile(".*(//d+)h.*");
+				Matcher matcher3 = hourRegex.matcher(data);
+				if (matcher3.matches())
+					hour = Integer.parseInt(matcher3.group(1));
+				
+				System.out.println(hour + ":" + minute + ":" + second + ":");
+				}
+
+			}
+
+	}
